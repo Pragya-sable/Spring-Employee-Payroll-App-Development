e21a142f66a01b7af638a88487e080e6dff541fe
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-24 23:49:58 +0530

    [Pragya] Add. Introducing Services Layer

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceImpl.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp.service;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +12,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    @Autowired
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+
 
     @Override
     public Employee createEmployee(EmployeeDTO employeeDTO) {
