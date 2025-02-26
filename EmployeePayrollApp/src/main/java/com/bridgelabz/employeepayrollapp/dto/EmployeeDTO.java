180f59517dbc3c6d3dd38b06aa8305bebecc1571
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-26 19:01:07 +0530

    [Pragya] Add. Added Validation

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDTO {
+    @NotEmpty(message = "Name cannot be empty")
+
+    @Min(value = 1, message = "ID must be greater than or equal to 1")
     private Long id;  // Use Long to match with the Entity
+    @Pattern(regexp = "^[A-Z][a-zA-Z ]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
     private double salary;
 }
